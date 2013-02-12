c61903bbda294b23c26ac5ba6078597bd8e7de73
Ryan Tanner <ryan.tanner@gmail.com>
2013-02-13 03:29:20 +0900

    Added redis URI logging

--- app/controllers/RedisClients.scala
+++ app/controllers/RedisClients.scala
@@ -11,6 +11,8 @@ object RedisClients {
 
   val maybeRedisURL = scala.util.Properties.envOrNone("REDISTOGO_URL")
 
+  Logger.info("Using redis url: " + maybeRedisURL)
+
   val (redisHost, redisPort) = maybeRedisURL match {
     case Some(redisURL) => {
       val redisURI = new java.net.URI(redisURL)
@@ -21,6 +23,8 @@ object RedisClients {
        Play.configuration.getInt("redis.port").getOrElse(6379))
     }
   }
+
+  Logger.info(s"Redis host: $redisHost, Redis port: $redisPort")
 
   val clientPool = new RedisClientPool(redisHost, redisPort)
 
