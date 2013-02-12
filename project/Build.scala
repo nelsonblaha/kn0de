3f047f99547b4fdb7ffc5c5015f31e4dc1b9803d
ryantanner <ryan.tanner@gmail.com>
2013-02-12 06:58:15 -0500

    Setting up heroku deploy

--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,14 @@ object ApplicationBuild extends Build {
   val appName         = "kn0de"
   val appVersion      = "1.0-SNAPSHOT"
     
-  lazy val authModule = Project("play20-auth-module", file("modules/auth"))
+//  lazy val authModule = Project("play20-auth-module", file("modules/auth"), settings = Defaults.defaultSettings ++ buildSettings)
+//   Seq(libraryDependencies ++= Seq("play" %% "play" % "2.1.0"), moduleConfigurations)
 
   val appDependencies = Seq(
     jdbc,
     anorm,
     "com.typesafe" %% "slick" % "1.0.0-RC2",
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
     "org.slf4j" % "slf4j-nop" % "1.6.4",
     "net.debasishg" % "redisclient_2.10" % "2.9",
     "org.webjars" % "bootstrap" % "2.2.2",
@@ -24,6 +26,7 @@ object ApplicationBuild extends Build {
     scalaVersion := "2.10.0",
     resolvers += "t2v.jp repo" at "http://www.t2v.jp/maven-repo/",
     resolvers += "jbcrypt repo" at "http://mvnrepository.com/"
-  ).dependsOn(authModule)
+  )
+//  ).dependsOn(authModule)
 
 }
